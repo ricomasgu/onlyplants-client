2dfd250e088b11f650938c93fa850195ffb9b14d
ricomasgu <ricard.comas.gomez@gmail.com>
2022-04-28 23:35:09 +0200

    Signup component not finished. onSubmit functionality and css need to be coded

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
   FormControl, 
   Input,
@@ -7,32 +7,74 @@ import {
 } from '@chakra-ui/react'
 
 const SignUp = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [avatarUrl, setAvatarUrl] = useState('')
+
+
+
   return (
     <form>
       <FormControl isRequired>
-        <Input id='first-name' placeholder='First Name' />
+        <Input 
+          id='first-name'
+          placeholder='First Name'
+          onChange={ (e) => setFirstName(e.target.value) }
+          value={ firstName }
+        />
       </FormControl>
 
       <FormControl isRequired>
-        <Input id='last-name' placeholder='Last Name' />
+        <Input
+          id='last-name'
+          placeholder='Last Name'
+          onChange={ (e) => setLastName(e.target.value) }
+          value={ lastName }
+        />
       </FormControl>
 
       <FormControl isRequired>
-        <Input id='email' type='email' placeholder='Email' />
+        <Input 
+          id='email' 
+          type='email' 
+          placeholder='Email' 
+          onChange={ (e) => setEmail(e.target.value) } 
+          value={ email }
+        />
       </FormControl>
 
       <FormControl isRequired>
-        <Input id='username' placeholder='Username' />
+        <Input 
+          id='username'
+          placeholder='Username'
+          onChange={ (e) => setUsername(e.target.value) }
+          value={ username }
+        />
       </FormControl>
 
       <FormControl isRequired>
-        <Input id='password' type='password' placeholder='*********' />
+        <Input 
+          id='password'
+          type='password'
+          placeholder='Password'
+          onChange={ (e) => setPassword(e.target.value) }
+          value={ password } />
       </FormControl>
 
-      <Avatar src="" />
-
+      <Avatar 
+        src={ avatarUrl }
+        name={ firstName === '' || lastName === '' ? null :  `${firstName} ${lastName}` }
+      />
+      
+      <Input
+        id='avatar'
+        type='file'
+        onChange={ (e) => setAvatarUrl(URL.createObjectURL(e.target.files[0])) }
+      />
       <Button variant='outline'>Create Account</Button>
-
     </form>
   )
 }
