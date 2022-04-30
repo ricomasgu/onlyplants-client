f52b6736e6dadb22abf95cd9cf8c11b714cd2d3c
guilhermefill <63548453+guilhermefill@users.noreply.github.com>
2022-04-30 12:49:17 +0200

    placeholder posts

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Routes, Route } from 'react-router-dom';
 import AddPost from './views/AddPost';
 import PostDetail from './views/PostDetail';
 import HomePlaceholder from './views/HomePlaceholder';
+import Feed from './views/Feed';
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<HomePlaceholder />} />
+				<Route path="/feed" element={<Feed />} />
 				<Route path="/post" element={<AddPost />} />
 				<Route path="/post/postdetail" element={<PostDetail />} />
 			</Routes>
