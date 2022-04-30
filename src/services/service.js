d7535a490de5fff0b5b5459993cfad8fbc7884d4
ricomasgu <ricard.comas.gomez@gmail.com>
2022-04-30 13:48:15 +0200

    Adding service for signup and login

---
+++ src/services/service.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+class Service {
+  constructor (){
+    this.baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:5005/api';
+  }
+
+  signup( userInput ) {
+    return axios.post(
+      `${ this.baseURL }/auth/signup`, 
+      userInput,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+        withCredentials: true
+      }
+    );
+  }
+
+  login( userInput ) {
+    return axios.post(
+      `${ this.baseURL }/auth/login`, 
+      userInput,
+      {
+        withCredentials: true
+      }
+    );
+  }
+}
+
+const service = new Service();
+export default service;
