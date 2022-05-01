c1b43b9c7d2270ed07e1014f520cb1c5b3a935db
ricomasgu <ricard.comas.gomez@gmail.com>
2022-05-01 19:43:20 +0200

    Adding a state to the main application

--- src/App.js
+++ src/App.js
@@ -9,14 +9,47 @@ import Login from './components/Login';
 import Feed from './views/Feed';
 
 function App() {
+	const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+	const userAppState = {
+		firstName,
+		lastName,
+		email,
+		username,
+		password,
+		avatar
+	}
+
+	const setUserState = ( userState ) => {
+		const {
+			firstName: firstNameAppState,
+			lastName: lastNameAppState,
+			email: emailAppState,
+			username: usernameAppState,
+			password: passwordAppState,
+			avatar: avatarAppState
+		} = userState;
+
+		setFirstName(firstNameAppState);
+		setLastName(lastNameAppState);
+		setEmail(emailAppState);
+		setUsername(usernameAppState);
+		setPassword(passwordAppState);
+		setAvatar(avatarAppState);
+	}
 
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<HomePlaceholder />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/feed" element={<Feed />} />
+				<Route path="/" element={<HomePlaceholder  />} />
+				<Route path="/signup" element={<SignUp setUserState={ setUserState } />} />
+				<Route path="/login" element={<Login setUserState={ setUserState } />} />
+				<Route path="/feed" element={<Feed { ...userAppState } />} />
 				<Route path="/post" element={<AddPost />} />
 				<Route path="/post/postdetail" element={<PostDetail />} />
 			</Routes>
