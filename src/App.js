02d307ff0ab414611eb1ba6be1150bf789ea260a
ricomasgu <ricard.comas.gomez@gmail.com>
2022-05-02 23:37:20 +0200

    What if we use two variables as an app state and with them we check the loggedin state and protect the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import SignUp from './components/SignUp';
 import AddPost from './views/AddPost';
 import PostDetail from './views/PostDetail';
-import HomePlaceholder from './views/HomePlaceholder';
 import Login from './components/Login';
 import Feed from './views/Feed';
+
+import service from './services/service';
+import { Spinner } from '@chakra-ui/react';
 
 function App() {
 	const [firstName, setFirstName] = useState('');
@@ -15,6 +17,8 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [avatar, setAvatar] = useState('');
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [loading, setLoading] = useState(true);
 
 	const userAppState = {
 		firstName,
@@ -41,18 +45,99 @@ function App() {
 		setUsername(usernameAppState);
 		setPassword(passwordAppState);
 		setAvatar(avatarAppState);
+		setLoggedIn(true);
 	};
+
+	const userLoggedIn = async () => {
+		try {
+			const resFromApi = await service.loggedIn();
+			if (resFromApi.data !== '' ) {
+				console.log(resFromApi.data);
+				setUserState( resFromApi.data );
+			}
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	
+	userLoggedIn();
 
 	return (
 		<div className="App">
-			<Routes>
-				<Route path="/" element={<HomePlaceholder  />} />
-				<Route path="/signup" element={<SignUp setUserState={ setUserState } />} />
-				<Route path="/login" element={<Login setUserState={ setUserState } />} />
-				<Route path="/feed" element={<Feed { ...userAppState } />} />
-				<Route path="/post" element={<AddPost />} />
-				<Route path="/post/postdetail" element={<PostDetail />} />
-			</Routes>
+			{	
+				loading ? 
+				<Spinner
+					thickness='4px'
+					speed='0.65s'
+					emptyColor='gray.200'
+					color='red.500'
+					size='xl'
+				/>
+					:
+				<Routes>
+					<Route
+						path="/"
+						element=
+						{
+							loggedIn ? 
+							<Navigate	to="/feed" /> 
+							: 
+							<Navigate to="/signup" /> 
+						}
+					/>
+					<Route 
+						path="/signup"
+						element=
+						{ 
+							!loggedIn ? 
+							<SignUp setUserState={ setUserState } /> 
+							: 
+							<Navigate to="/feed" /> 
+						}
+					/>
+					<Route 
+						path="/login" 
+						element=
+						{ 
+							!loggedIn ? 
+							<Login setUserState={ setUserState } /> 
+							: 
+							<Navigate to="/feed" /> 
+						}
+					/>
+					<Route 
+						path="/feed" 
+						element=
+						{ 
+							loggedIn ? 
+							<Feed { ...userAppState } /> 
+							: 
+							<Navigate to="/signup" /> 
+						} 
+					/>
+					<Route 
+						path="/post" 
+						element=
+						{
+							loggedIn ? 
+							<AddPost /> 
+							: 
+							<Navigate to="/signup" />
+						} 
+					/>
+					<Route 
+						path="/post/postdetail" 
+						element=
+						{ 
+							loggedIn ? 
+							<PostDetail /> 
+							: 
+							<Navigate to="/signup" /> 
+						} 
+					/>
+				</Routes>
+			}
 		</div>
 	);
 }
