6c1caeacc3ce55af76a146829db5ec4175adb193
guilhermefill <63548453+guilhermefill@users.noreply.github.com>
2022-05-14 11:38:48 +0200

    Dark mode working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,21 @@ import {
 	Avatar,
 	Center,
 	Button,
+	useColorMode,
+	Switch,
+	FormLabel,
+	Text,
+	useColorModeValue,
 } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
 import service from '../services/service';
 
 const Navbar = (props) => {
 	const { username, avatar } = props.userState;
-
+	const { colorMode, toggleColorMode } = useColorMode();
 	const navigate = useNavigate();
+
+	const color = useColorModeValue('black', 'gray.800');
 
 	const handleLogout = async () => {
 		try {
@@ -24,21 +31,26 @@ const Navbar = (props) => {
 
 			props.setUserState('');
 			props.setLoggedIn(false);
-			
+
 			navigate('/');
-		} 
-		catch (error) {
+		} catch (error) {
 			console.log(error);
 		}
 	};
 	return (
 		<div className="navbar">
 			<nav>
-				<Flex alignItems="center">
+				<Flex alignItems="center" color={color}>
 					<Spacer />
 					<HStack spacing="24px">
 						<Box p="2">
-							<Heading size="md">OnlyPlants</Heading>
+							<Center gap="2">
+								<Avatar
+									src="https://upload.wikimedia.org/wikipedia/commons/3/35/Berry_plant_illustration.png"
+									size="sm"
+								/>
+								<Heading size="md">OnlyPlants</Heading>
+							</Center>
 						</Box>
 						<Box>
 							<Link to="/feed">Feed</Link>
@@ -60,6 +72,12 @@ const Navbar = (props) => {
 									<Button onClick={handleLogout} size="sm">
 										Logout
 									</Button>
+									<FormLabel htmlFor="toggle-mode" mb="0">
+										<Text fontSize="12px">
+											Mode: {colorMode === 'light' ? 'Light' : 'Dark'}
+										</Text>
+									</FormLabel>
+									<Switch onChange={toggleColorMode}></Switch>
 								</Center>
 							</Wrap>
 						</Center>
