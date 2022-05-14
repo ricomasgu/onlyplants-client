4a39436d24362b63f23cab456409ca66ba7c4d4e
guilhermefill <63548453+guilhermefill@users.noreply.github.com>
2022-05-14 11:46:52 +0200

    fix services route

--- src/views/UserDetails.jsx
+++ src/views/UserDetails.jsx
@@ -1,38 +1,39 @@
 import React, { useEffect, useParams, useState } from 'react';
 
-import postService from '../api/postServices';
+import postService from '../services/postServices';
 import Loading from '../components/Loading';
 import UserProfile from '../components/UserProfile';
 
 const UserDetails = () => {
-  const userId = useParams();
+	const userId = useParams();
 
-  const [userInfo, setUserInfo] = useState();
-  const [loading, setLoading] = useState(true); 
+	const [userInfo, setUserInfo] = useState();
+	const [loading, setLoading] = useState(true);
 
-  const getUserInfo = async () => {
-    try {
-      const user = await postService.getUser(userId);
-      setUserInfo(user);
-    } catch (error) {
-      console.log(error);
-    } 
-    setLoading(false);
-  };
-  
-  useEffect( () => {
-    getUserInfo();
-  }, []);
+	const getUserInfo = async () => {
+		try {
+			const user = await postService.getUser(userId);
+			setUserInfo(user);
+		} catch (error) {
+			console.log(error);
+		}
+		setLoading(false);
+	};
 
-  return (
-    <>
-      { loading ? 
-        <Loading/>
-        :
-        <UserProfile userInfo={userInfo} itIsMe={false} />
-      }
-    </>
-  );
-}
+	useEffect(() => {
+		getUserInfo();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-export default UserDetails
+	return (
+		<>
+			{loading ? (
+				<Loading />
+			) : (
+				<UserProfile userInfo={userInfo} itIsMe={false} />
+			)}
+		</>
+	);
+};
+
+export default UserDetails;
