36d2a3ce9cc540b2bbb3b603f6aca37a7c2c1c6e
guilhermefill <63548453+guilhermefill@users.noreply.github.com>
2022-07-12 15:32:56 -0300

    minlength

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,130 +1,135 @@
 import React, { useState } from 'react';
-import { 
-  FormControl, 
-  Input,
-  Avatar,
-  Button,
-  Box,
-  Center,
-  Container,
-  VStack,
-  Text
+import {
+	FormControl,
+	Input,
+	Avatar,
+	Button,
+	Box,
+	Center,
+	Container,
+	VStack,
+	Text,
 } from '@chakra-ui/react';
 import { NavLink, useNavigate } from 'react-router-dom';
 
 import authService from '../services/authServices';
 
-const SignUp = ( props ) => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [avatar, setAvatar] = useState('');
+const SignUp = (props) => {
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [avatar, setAvatar] = useState('');
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const onSubmit = async ( event ) => {
-    event.preventDefault();
+	const onSubmit = async (event) => {
+		event.preventDefault();
 
-    try {
-      const resFromApi = await authService.signup({
-        firstName,
-        lastName,
-        email,
-        username,
-        password,
-        avatar
-      });
+		try {
+			const resFromApi = await authService.signup({
+				firstName,
+				lastName,
+				email,
+				username,
+				password,
+				avatar,
+			});
 
-      console.log( resFromApi );
+			console.log(resFromApi);
 
-      props.setUserState( resFromApi.data );
-      props.setLoggedIn( true );
+			props.setUserState(resFromApi.data);
+			props.setLoggedIn(true);
 
-      navigate('/feed');
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+			navigate('/feed');
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <Container w='100vw' h='100vh'>
-      <Center w='100%' h='100%'>
-        <Box p={5} shadow='md' borderWidth='1px'>
-          <form onSubmit={ onSubmit } >
-            <VStack>
-              <FormControl isRequired>
-                <Input 
-                  id='first-name'
-                  placeholder='First Name'
-                  onChange={ (e) => setFirstName(e.target.value) }
-                  value={ firstName }
-                />
-              </FormControl>
+	return (
+		<Container w="100vw" h="100vh">
+			<Center w="100%" h="100%">
+				<Box p={5} shadow="md" borderWidth="1px">
+					<form onSubmit={onSubmit}>
+						<VStack>
+							<FormControl isRequired>
+								<Input
+									id="first-name"
+									placeholder="First Name"
+									onChange={(e) => setFirstName(e.target.value)}
+									value={firstName}
+								/>
+							</FormControl>
 
-              <FormControl isRequired>
-                <Input
-                  id='last-name'
-                  placeholder='Last Name'
-                  onChange={ (e) => setLastName(e.target.value) }
-                  value={ lastName }
-                />
-              </FormControl>
+							<FormControl isRequired>
+								<Input
+									id="last-name"
+									placeholder="Last Name"
+									onChange={(e) => setLastName(e.target.value)}
+									value={lastName}
+								/>
+							</FormControl>
 
-              <FormControl isRequired>
-                <Input 
-                  id='email' 
-                  type='email' 
-                  placeholder='Email' 
-                  onChange={ (e) => setEmail(e.target.value) } 
-                  value={ email }
-                />
-              </FormControl>
+							<FormControl isRequired>
+								<Input
+									id="email"
+									type="email"
+									placeholder="Email"
+									onChange={(e) => setEmail(e.target.value)}
+									value={email}
+								/>
+							</FormControl>
 
-              <FormControl isRequired>
-                <Input 
-                  id='username'
-                  placeholder='Username'
-                  onChange={ (e) => setUsername(e.target.value) }
-                  value={ username }
-                />
-              </FormControl>
+							<FormControl isRequired>
+								<Input
+									id="username"
+									placeholder="Username"
+									onChange={(e) => setUsername(e.target.value)}
+									value={username}
+								/>
+							</FormControl>
 
-              <FormControl isRequired>
-                <Input 
-                  id='password'
-                  type='password'
-                  placeholder='Password'
-                  onChange={ (e) => setPassword(e.target.value) }
-                  value={ password } />
-              </FormControl>
+							<FormControl isRequired>
+								<Input
+									id="password"
+									type="password"
+									placeholder="Password"
+									onChange={(e) => setPassword(e.target.value)}
+									value={password}
+									minlength="8"
+								/>
+							</FormControl>
 
-              <Avatar 
-                src={ avatar !== '' ? URL.createObjectURL( avatar ) : avatar }
-                name={ firstName === '' || lastName === '' ? null :  `${firstName} ${lastName}` }
-              />
+							<Avatar
+								src={avatar !== '' ? URL.createObjectURL(avatar) : avatar}
+								name={
+									firstName === '' || lastName === ''
+										? null
+										: `${firstName} ${lastName}`
+								}
+							/>
 
-              <Input
-                id='avatar'
-                type='file'
-                onChange={ (e) => setAvatar(e.target.files[0]) }
-              />
+							<Input
+								id="avatar"
+								type="file"
+								onChange={(e) => setAvatar(e.target.files[0])}
+							/>
 
-              <Button type='submit' colorScheme='green' >
-                Create Account
-              </Button>
+							<Button type="submit" colorScheme="green">
+								Create Account
+							</Button>
 
-              <NavLink to='/login' >
-                <Text color='green.500'> Already have an account? </Text>
-              </NavLink>
-            </VStack>
-          </form>
-        </Box>
-      </Center>
-    </Container>
-  );
-}
+							<NavLink to="/login">
+								<Text color="green.500"> Already have an account? </Text>
+							</NavLink>
+						</VStack>
+					</form>
+				</Box>
+			</Center>
+		</Container>
+	);
+};
 
-export default SignUp
+export default SignUp;
